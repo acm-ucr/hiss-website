da568abe34e03aa4c3314a2c6d3c82dd683f30a4
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-10-13 13:56:37 -0700

    changed to purple

--- src/components/RedBox.jsx
+++ src/components/RedBox.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const RedBox = () => {
-  return <div className="h-24 w-24 bg-hiss-blue">redBox</div>;
+  return <div className="h-24 w-24 bg-hiss-purple">redBox</div>;
 };
 
 export default RedBox;
