cc7743702339d90b874c5e1d349bf8964eefab3a
Aldrie Barcena <aldrievbarcena@gmail.com>
2023-10-19 16:17:57 -0700

    added parameters for custom text, issues to be asked: positioning, adding mail logo, adding image

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,16 @@
 import Card from "../components/board/Card";
 
 const Home = () => {
-  return <Card />;
+  return (
+    <Card
+      name="Aldrie Barcena"
+      title="Developer"
+      year="1st year"
+      major="Comp Eng"
+      pronouns="He/Him"
+      linkedIn="https://youtube.com"
+    />
+  );
 };
 
 export default Home;
--- src/components/board/Card.jsx
+++ src/components/board/Card.jsx
@@ -1,23 +1,35 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
+  const { name, title, year, major, pronouns, linkedIn } = props;
+
   return (
-    <div className="cardContainer font-poppins m-4 bg-white w-32 h-52 rounded-md shadow p-3">
-      <div className="photoContainer bg-gray-500 w-24 h-24 rounded-lg"></div>
-
-      <div className="textContainer">
-        <div className="name mt-1 font-bold text-md text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
-          Jennie Kim
+    <div className="cardContainer font-poppins m-4 bg-white w-72 h-96 rounded-md shadow flex flex-col items-center justify-center">
+      <div className="contentContainer w-60 h-full flex flex-col justify-center items-center text-clip">
+        <div className="photoContainer bg-gray-500 w-full h-1/2 rounded-lg">
+          <img src="../public/images/judge.png" alt="" />
         </div>
 
-        <div className="title text-sm font-light">President</div>
+        <div className="textContainer">
+          <div className="name mt-2 font-bold text-3xl text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+            {name}
+          </div>
 
-        <div className="linkedinContainer text-xs text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
-          <div>in-logo image</div>
+          <div className="title -mt-0 text-2xl font-light">{title}</div>
+
+          <div className="linkedinContainer ">
+            <a href={linkedIn} className="no-underline">
+              <div className="name ml-1 font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+                in
+              </div>
+            </a>
+          </div>
+
+          <div className="education text-md">
+            {year} - {major}
+          </div>
+          <div className="pronouns text-md">Pronouns: {pronouns}</div>
         </div>
-
-        <div className="education text-xxs">3rd year - Comp Sci</div>
-        <div className="pronouns text-xxs">Pronouns: She/Her/Hers</div>
       </div>
     </div>
   );
