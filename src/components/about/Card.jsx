155e72b3b1e64394ffc824e8e7ddb7d9f4f7bef8
achen409 <andrewr.chen@gmail.com>
2023-10-20 21:19:26 -0700

    Add more styling

--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -3,13 +3,15 @@ import { FaDiscord } from "react-icons/fa";
 
 const Card = () => {
   return (
-    <div className="bg-red-200 flex flex-col items-center justify-center w-40 h-1/5 rounded-lg">
-      <FaDiscord className="text-4xl md:text-6xl" />
-      <div className="bg-hiss-yellow h-2 w-1/6 rounded-lg" />
-      <p className="text-xl bg-gradient-to-r from-hiss-blue to-hiss-purple bg-clip-text text-transparent">
-        Discord
-      </p>
-      <button className="w-3/5 border-1 border-black bg-white rounded-full flex items-center justify-center">
+    <div className="bg-white flex flex-col items-center justify-top pt-4 w-48 h-1/4 rounded-lg">
+      <div className="space-y-1 flex flex-col items-center justify-center">
+        <FaDiscord className="text-7xl" />
+        <div className="bg-hiss-yellow h-2 w-2/6 rounded-lg" />
+        <p className="mt-2 text-xl font-bold bg-gradient-to-r from-hiss-blue to-hiss-purple bg-clip-text text-transparent">
+          Discord
+        </p>
+      </div>
+      <button className="w-3/5 border-1 border-black bg-white rounded-full flex items-center justify-center mt-6">
         link
       </button>
     </div>
