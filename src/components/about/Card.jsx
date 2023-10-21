28871b32ba789f2a1af4dcbfdfeb29421040d8da
achen409 <andrewr.chen@gmail.com>
2023-10-20 20:28:55 -0700

    Added initial elements

--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { FaDiscord } from "react-icons/fa";
 
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="bg-red-200 flex flex-col items-center justify-center w-40 h-1/5 rounded-lg">
+      <FaDiscord className="text-4xl md:text-6xl" />
+      <div className="bg-hiss-yellow h-2 w-1/6 rounded-lg" />
+      <p className="text-xl bg-gradient-to-r from-hiss-blue to-hiss-purple bg-clip-text text-transparent">
+        Discord
+      </p>
+      <button className="w-3/5 border-1 border-black bg-white rounded-full flex items-center justify-center">
+        link
+      </button>
+    </div>
+  );
 };
 
 export default Card;
