2cbed326e6a4aacf9010b82d2e6d9b4e514bbea9
achen409 <andrewr.chen@gmail.com>
2023-10-21 19:28:18 -0700

    changed page.js back to default

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,5 @@
-import Card from "@/components/about/Card";
-
 const Home = () => {
-  return (
-    <div className="bg-white w-full flex flex-col justify-center items-center h-screen">
-      <Card />
-    </div>
-  );
+  return <>HISS Website</>;
 };
 
 export default Home;
