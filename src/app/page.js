8cec364a78dc08c175b192af4cc0065ee9235add
achen409 <andrewr.chen@gmail.com>
2023-10-22 23:49:03 -0700

    added props

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
+import Card from "@/components/about/Card";
+import { FaDiscord } from "react-icons/fa";
+
 const Home = () => {
-  return <>HISS Website</>;
+  return (
+    <div className="bg-white w-full flex flex-col justify-center items-center h-screen">
+      <Card
+        text={"Discord"}
+        link={"www.google.com"}
+        icon={<FaDiscord className="text-7xl text-hiss-purple" />}
+      />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/about/Card.jsx
+++ src/components/about/Card.jsx
@@ -1,18 +1,20 @@
+import Link from "next/link";
 import React from "react";
-import { FaDiscord } from "react-icons/fa";
 
-const Card = () => {
+const Card = ({ text, icon, link }) => {
   return (
     <div className="bg-white flex flex-col items-center justify-top pt-4 pb-12 w-48 h-1/4 rounded-lg border-1 shadow-lg">
       <div className="space-y-1 flex flex-col items-center justify-center">
-        <FaDiscord className="text-7xl text-hiss-purple" />
+        {icon}
         <div className="bg-hiss-yellow h-2 w-2/6 rounded-lg" />
         <p className="mt-2 text-xl font-bold bg-gradient-to-r from-hiss-blue to-hiss-purple bg-clip-text text-transparent">
-          Discord
+          {text}
         </p>
       </div>
       <button className="w-3/5 border-1 border-black bg-white rounded-full flex items-center justify-center mt-6">
-        link
+        <Link href={link} className="text-black no-underline">
+          Join
+        </Link>
       </button>
     </div>
   );
