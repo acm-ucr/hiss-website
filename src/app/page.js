972f046edf249f549567e105278073545eee6f42
achen409 <andrewr.chen@gmail.com>
2023-10-22 23:50:15 -0700

    page.js back to normal

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,5 @@
-import Card from "@/components/about/Card";
-import { FaDiscord } from "react-icons/fa";
-
 const Home = () => {
-  return (
-    <div className="bg-white w-full flex flex-col justify-center items-center h-screen">
-      <Card
-        text={"Discord"}
-        link={"www.google.com"}
-        icon={<FaDiscord className="text-7xl text-hiss-purple" />}
-      />
-    </div>
-  );
+  return <>HISS Website</>;
 };
 
 export default Home;
