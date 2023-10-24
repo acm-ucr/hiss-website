028631fcfdf46fa3dd7118912047d683508ce325
Aldrie Barcena <aldrievbarcena@gmail.com>
2023-10-24 16:29:14 -0700

    removed unneccessary divs & classnames

--- src/components/board/Card.jsx
+++ src/components/board/Card.jsx
@@ -7,36 +7,32 @@ import Link from "next/link";
 
 const Card = ({ name, title, year, major, pronouns, linkedIn, email }) => {
   return (
-    <div className="font-poppins m-4 bg-white w-64 h-96 rounded-3xl shadow flex flex-col items-center justify-center">
-      <div className="w-56 h-5/6 flex flex-col justify-center items-start">
-        <div className="flex w-full h-48">
-          <Image
-            src={Judge}
-            alt="Picture of the author"
-            className="w-full h-full rounded-2xl"
-          />
+    <div className="font-poppins m-4 p-4 bg-white w-64 h-96 rounded-3xl shadow flex flex-col items-start justify-center">
+      <Image
+        src={Judge}
+        alt="Picture of the author"
+        className="w-full h-2/3 rounded-2xl"
+      />
+
+      <div>
+        <div className="mt-2 font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+          {name}
         </div>
 
-        <div className="textContainer">
-          <div className="mt-2 font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
-            {name}
-          </div>
+        <div className="-mt-0 text-2xl font-light">{title}</div>
 
-          <div className="-mt-0 text-2xl font-light">{title}</div>
+        <Link href={linkedIn}>
+          <FaLinkedinIn className="inline h-6 w-6 ml-2 mb-1 text-hiss-purple" />
+        </Link>
 
-          <Link href={linkedIn}>
-            <FaLinkedinIn className="inline h-6 w-6 ml-2 mb-1 text-hiss-purple" />
-          </Link>
+        <Link href={email}>
+          <GrMail className="inline h-6 w-6 ml-2 mb-1 rounded-xl text-hiss-purple" />
+        </Link>
 
-          <Link href={email}>
-            <GrMail className="inline h-6 w-6 ml-2 mb-1 rounded-xl text-hiss-purple" />
-          </Link>
-
-          <div className="text-base">
-            {year} - {major}
-          </div>
-          <div className="text-base">Pronouns: {pronouns}</div>
+        <div className="text-base">
+          {year} - {major}
         </div>
+        <div className="text-base">Pronouns: {pronouns}</div>
       </div>
     </div>
   );
