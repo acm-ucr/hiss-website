384db879343e7c8f3cf050ec36ba993cca984b30
Falak Tulsi <falaktulsi@gmail.com>
2023-10-27 03:02:19 -0700

    Fixed sizes and flexbox formatting

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -2,19 +2,26 @@ import React from "react";
 
 const Event = (props) => {
   return (
-    <div className="font-poppins m-4 p-4 bg-white w-64 h-96 rounded-3xl shadow flex flex-col items-start justify-center">
-      <div className="mt-2 font-bold text-2xl text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
-          {props.date.getMonth()}
-          {props.date.getDate()}
-          {props.date.getHours() + ":" + props.date.getMinutes()}
+    <div className="mt-4 ml-4 font-poppins bg-white rounded-xl w-8/12 h-30 min-h-full gap-x-3  shadow flex flex-row items-start justify-center">
+      <div className="w-fit h-fit  rounded-l-xl  bg-gradient-to-r from-hiss-blue to-hiss-purple">
+          <div className="w-32 h-36 font-bold text-4xl text-white flex flex-col gap-y-1 items-center justify-center">
+            <div>{/* {props.date.getMonth()} */ "OCT"}</div>
+            <div> {/* {props.date.getDate()} */ "27"}</div>
+            <div className="font-extralight text-sm"> {/* {props.date.getHours() + ":" + props.date.getMinutes()} */ "2:07AM"}</div>
+          </div>
         </div>
-        <div className="mt-2 font-bold text-2xl">
-          {props.title}
-          {"zoom: " + props.link}
-          {props.description}
+        <div className=" h-36 font-bold text-4xl flex flex-col gap-y-1 items-start justify-center">
+        <div className="font-bold text-4xl">
+          {/* {props.title} */ "Test Event"}
+          <div className="mt-2 font-light text-sm">
+              {/* {"zoom: " + props.link} */ "zoom: link"}<br></br>
+          </div>
         </div>
-    </div>
-  );
+        <div className="mt-2 font-light text-sm">
+          {/* {props.description} */ "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation "}
+        </div>
+        </div>
+    </div>);
 };
 
 export default Event;
