7fc3278982e38377d110d44347a01ef8780b4874
Piyali <Piyali@ucr-secure-36-10-13-195-55.wnet.ucr.edu>
2023-10-27 15:04:36 -0700

    Created Headers

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ text }) => {
+  return (
+    <div className="flex-col items-center justify-center ml-1 inline-flex">
+      <p className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+        {" "}
+        {text}{" "}
+      </p>
+      <div className="w-24 h-2.5 bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-lg" />
+    </div>
+  );
 };
 
 export default Header;
