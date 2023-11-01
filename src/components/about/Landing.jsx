a1a714ee22a2f468521017819f2a2abed2532e40
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-10-31 23:28:41 -0700

    slight changes

--- src/components/about/Landing.jsx
+++ src/components/about/Landing.jsx
@@ -9,15 +9,17 @@ const Landing = () => {
           <Image src={chart} className="h-1/2 w-auto m-0" alt="chart" />
         </div>
         <div className="flex-1 relative right-24">
-          <div className="text-5xl  py-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
-            Highlander Statistics Society
-          </div>
-          <div className=" my-1 text-white px-0 py-2.5 font-bold text-center text-3xl h-14 w-[77vh] bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full">
-            at Unversity of Calirfornia Riverside
-          </div>
-          <div className="my-1 h-10 w-6/12">
-            Lorem ipsum dolor sit amet, consectetur hadipiscing elit, sed do
-            eiusmod tempor incididunt ut labore
+          <div className="flex flex-col w-3/4 lg:w-auto">
+            <p className="text-5xl  py-0 font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+              Highlander Statistics Society
+            </p>
+            <p className=" my-0 text-white px-0 py-2.5 font-bold text-center text-3xl h-14 w-[77vh] bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full">
+              at Unversity of Calirfornia Riverside
+            </p>
+            <p className="my-1 h-10 w-6/12">
+              Lorem ipsum dolor sit amet, consectetur hadipiscing elit, sed do
+              eiusmod tempor incididunt ut labore
+            </p>
           </div>
         </div>
       </div>
