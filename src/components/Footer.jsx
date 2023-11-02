9f5497b552b75776f5b1c8b0523c7d12e158ae6f
Kavin Phabiani <kavinphab@gmail.com>
2023-11-02 15:18:17 -0700

    formatting

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import { FaInstagram, FaDiscord } from "react-icons/fa";
 import { IoMail } from "react-icons/io5";
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full  mx-auto p-4 items-baseline">
-      <div className="flex items-center font-poppins font-bold text-3xl text-white ">
+    <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full p-4">
+      <div className="flex items-center font-poppins font-bold text-3xl text-white">
         HISS
-        <div className=" w-full max-h-full flex space-x-4 place-content-end">
+        <div className="w-full max-h-full flex space-x-4 place-content-end">
           {<FaInstagram className="text-2xl" />}{" "}
           {<FaDiscord className="text-2xl" />} {<IoMail className="text-2xl" />}
         </div>
