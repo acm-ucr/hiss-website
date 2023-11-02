20a8d44198e4df1749e80589cc0b89579e4b0c3c
Kavin Phabiani <kavinphab@gmail.com>
2023-11-01 19:19:53 -0700

    added icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
+import { FaInstagram, FaDiscord } from "react-icons/fa";
+import {IoMail} from "react-icons/io5";
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full max-w-screen-xl mx-auto p-4 md:py-8">
-      <div className="sm:flex sm:items-center sm:justify-between font-poppins font-bold text-3xl text-white ">
-        HISS <FaInstagram />
-      </div>
+    <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full max-w-screen-xl mx-auto p-4 items-baseline">
+      <div className="flex items-center font-poppins font-bold text-3xl text-white ">
+        HISS
+        <div className= " w-full max-h-full flex space-x-4 place-content-end">
+        {<FaInstagram className="text-2xl" />} {<FaDiscord className="text-2xl" />} {<IoMail className="text-2xl" />}</div>
+        </div> 
     </div>
   );
 };
