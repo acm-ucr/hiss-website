07dbb4717ace2dc231220bc2ded287a5d09235c4
Kavin Phabiani <kavinphab@gmail.com>
2023-11-02 15:10:30 -0700

    extended to fit screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { FaInstagram, FaDiscord } from "react-icons/fa";
-import {IoMail} from "react-icons/io5";
+import { IoMail } from "react-icons/io5";
 const Footer = () => {
   return (
-    <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full max-w-screen-xl mx-auto p-4 items-baseline">
+    <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full  mx-auto p-4 items-baseline">
       <div className="flex items-center font-poppins font-bold text-3xl text-white ">
         HISS
-        <div className= " w-full max-h-full flex space-x-4 place-content-end">
-        {<FaInstagram className="text-2xl" />} {<FaDiscord className="text-2xl" />} {<IoMail className="text-2xl" />}</div>
-        </div> 
+        <div className=" w-full max-h-full flex space-x-4 place-content-end">
+          {<FaInstagram className="text-2xl" />}{" "}
+          {<FaDiscord className="text-2xl" />} {<IoMail className="text-2xl" />}
+        </div>
+      </div>
     </div>
   );
 };
