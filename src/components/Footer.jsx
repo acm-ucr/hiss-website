3468d5bb0eff1d086c5b4f8e74b9263eee8800b9
Kavin Phabiani <kavinphab@gmail.com>
2023-11-03 13:30:01 -0700

    added links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,16 @@ const Footer = () => {
     <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple w-full p-4">
       <div className="flex items-center font-poppins font-bold text-3xl text-white">
         HISS
-        <div className="w-full max-h-full flex space-x-4 place-content-end">
+        <div className="w-full max-h-full flex space-x-4 place-content-end ">
+          <a href="https://www.google.com/" className="text-white">
           {<FaInstagram className="text-2xl" />}{" "}
-          {<FaDiscord className="text-2xl" />} {<IoMail className="text-2xl" />}
+          </a>
+          <a href="https://www.google.com/" className="text-white">
+          {<FaDiscord className="text-2xl" />} 
+          </a>
+          <a href="https://www.google.com/" className="text-white">
+          {<IoMail className="text-2xl" />}
+          </a>
         </div>
       </div>
     </div>
