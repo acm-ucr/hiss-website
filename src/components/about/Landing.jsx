91634290d4b2150e9d2403be897d3daff07b2e97
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-11-04 22:41:48 -0700

    fixed small spacev2

--- src/components/about/Landing.jsx
+++ src/components/about/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = () => {
           </div>
           <div className="px-2 flex w-full justify-center items-start">
             <div className="flex flex-col w-3/4 lg:w-auto">
-              <p className="text-5xl py-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
+              <p className="text-5xl py-1.5 font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
                 Highlander Statistics Society
               </p>
               <p className="mt-0 my-0 text-white px-0 py-2.5 font-bold text-center text-3xl h-14 w-[83vh] bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full">
