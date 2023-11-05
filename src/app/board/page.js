2045dc18aaca9b9271a204d512d149c51e90d2f1
SafeDuck <schopra579@gmail.com>
2023-11-04 18:48:09 -0700

    more setup

--- src/app/board/page.js
+++ src/app/board/page.js
@@ -4,7 +4,9 @@ const Home = () => {
   return (
     <>
       <Title title={"Meet The Team"} />
-      <Cards />
+      <div className="flex flex-col justify-center items-center">
+        <Cards />
+      </div>
     </>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import Cards from "@/components/about/Cards";
 import Events from "@/components/Events";
 const Home = () => {
   return (
-    <>
+    <div className="flex flex-col justify-center items-center">
       <Landing />
       <HISS />
       <Cards />
       <Events />
-    </>
+    </div>
   );
 };
 export default Home;
