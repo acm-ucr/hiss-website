2aece0a16f5cce7157d3137ffddcbe0022015426
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-11-04 22:30:57 -0700

    fixed widths

--- src/components/about/Landing.jsx
+++ src/components/about/Landing.jsx
@@ -3,18 +3,18 @@ import Image from "next/image";
 import chart from "../../../public/assets/chart.svg";
 const Landing = () => {
   return (
-    <div className="w-full flex justify-start items-end h-[70vh]">
+    <div className="w-full flex justify-center items-end h-[70vh]">
       <div className="flex-row w-auto">
         <div className="h-auto flex gap-8 ">
-          <div className="flex w-full justify-end">
+          <div className="flex w-1/2 justify-end items-center">
             <Image src={chart} className="h-full w-auto m-0" alt="chart" />
           </div>
-          <div className="px-3 flex w-full justify-center items-start">
+          <div className="px-1 flex w-full justify-center items-start">
             <div className="flex flex-col w-3/4 lg:w-auto">
               <p className="text-5xl py-1 font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
                 Highlander Statistics Society
               </p>
-              <p className="mt-0 my-0 text-white px-0 py-2.5 font-bold text-center text-3xl h-14 w-[77vh] bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full">
+              <p className="mt-0 my-0 text-white px-0 py-2.5 font-bold text-center text-3xl h-14 w-[83vh] bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full">
                 at Unversity of Calirfornia Riverside
               </p>
               <p className="my-1 py-1 h-10 w-6/12">
