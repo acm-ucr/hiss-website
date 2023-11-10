4eaf46e17ebfc8fb2d57aa13d80312a9364961c9
Kavin Phabiani <kavinphab@gmail.com>
2023-11-09 22:50:15 -0800

    centered items

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,8 +6,8 @@ const Title = ({ title }) => {
       <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple mb-2.5 mt-14 rounded-r-full h-3 sm:h-5 w-1/4 "></div>
       <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple mb-2.5 rounded-r-full h-3 sm:h-5 w-1/3"></div>
       <div
-        className="text-white text-right  font-poppins font-bold bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-r-full 
-      text-sm h-8 sm:text-3xl sm:h-16 w-2/5 pr-5 sm:pr-7 pt-1"
+        className="text-white text-right font-poppins font-bold bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-r-full 
+      text-sm h-8 sm:text-3xl sm:h-16 w-2/5 pr-6 sm:pr-12 flex items-center justify-end"
       >
         {title}
       </div>
