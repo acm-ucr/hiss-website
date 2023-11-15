2ea7a08ba642efd90f5e1bcfb5bd891eb98eb002
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-11-14 17:56:10 -0800

    calendar donev2

--- src/components/calendar/CustomHeader.jsx
+++ src/components/calendar/CustomHeader.jsx
@@ -1,6 +1,6 @@
 const Header = (props) => {
   return (
-    <div className="pt-2 h-12 md:text-2xl bg-gradient-to-r from-hiss-blue to-hiss-purple  text-white ">
+    <div className="pt-2 h-12 font-semibold md:text-2xl bg-gradient-to-r from-hiss-blue to-hiss-purple  text-white ">
       {props.label}
     </div>
   );
