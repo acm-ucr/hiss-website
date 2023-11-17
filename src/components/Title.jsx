5c9ed2a00eee11872dc396d0542f8923a6741696
Kavin Phabiani <kavinphab@gmail.com>
2023-11-17 11:28:10 -0800

    remmoved redundancy

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ title }) => {
   return (
     <div>
-      <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple mb-2.5 mt-14 rounded-r-full h-5 sm:h-5 w-1/4 sm:w-1/4 "></div>
+      <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple mb-2.5 mt-14 rounded-r-full h-5 sm:h-5 w-1/4 "></div>
       <div className="bg-gradient-to-r from-hiss-blue to-hiss-purple mb-2.5 rounded-r-full h-5 sm:h-5 w-2/5 sm:w-1/3"></div>
       <div
         className="text-white text-right font-poppins font-bold bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-r-full 
