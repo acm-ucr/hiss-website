27bdc4487eba5f5ca60661b1f499fb647e79d6c2
Piyali <Piyali@ucr-secure-36-10-13-193-173.wnet.ucr.edu>
2023-11-17 13:57:56 -0800

    Fixed image & text formatting in HISS.jsx

--- src/components/about/HISS.jsx
+++ src/components/about/HISS.jsx
@@ -5,42 +5,27 @@ import About from "../../../public/images/about.png";
 
 const HISS = () => {
   return (
-    <div className="flex-col items-center justify-center ml-1 inline-flex">
+    <div className="flex-col items-center justify-center inline-flex gap-10 w-1/2">
       <Header text="About HISS" />
       <p>
         The Highlander Statistics Society (HiSS) is a student organization whose
-        <br></br>
         purpose is to form unity and friendship among students majoring or
-        <br></br>
         interested in statistics. HiSS works to promote scholarship and interest
-        in<br></br>
-        statistics while informing people about opportunities and challenges
-        that are<br></br>
-        present in the field. Through social, academic, and service events, HiSS
-        strives<br></br>
-        to build a strong community among its club members in addition to
-        <br></br>
-        encouraging involvement and success in statistics.
+        in statistics while informing people about opportunities and challenges
+        that are present in the field. Through social, academic, and service
+        events, HiSS strives to build a strong community among its club members
+        in addition to encouraging involvement and success in statistics.
       </p>
-      <Image
-        className="rounded-full w-4/6 h-1/3 py-5 px-10"
-        src={About}
-        alt="HISS Image"
-      />
+      <Image className="rounded-full w-5/6 " src={About} alt="HISS Image" />
       <p>
         The Highlander Statistics Society (HiSS) aims to bring together students
-        who<br></br>
-        are interested in statistics across various disciplines. The club was
-        founded in<br></br>
-        Spring 2010 and has been a consistent source of assistance and
-        opportunity<br></br>
-        for undergraduates. HiSS works to promote academic achievement by
-        <br></br>
-        fostering a supportive environment where students can learn and
-        collaborate<br></br>
-        together. Club activities include R and Python coding workshops, social
-        events,<br></br>
-        community service, and participation in the annual Datafest competition.
+        who are interested in statistics across various disciplines. The club
+        was founded in Spring 2010 and has been a consistent source of
+        assistance and opportunity for undergraduates. HiSS works to promote
+        academic achievement by fostering a supportive environment where
+        students can learn and collaborate together. Club activities include R
+        and Python coding workshops, social events, community service, and
+        participation in the annual Datafest competition.
       </p>
     </div>
   );
