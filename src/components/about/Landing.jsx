3c477047e655f810eeb837c43f7d3a1efd04f925
SafeDuck <schopra579@gmail.com>
2023-11-22 22:00:21 -0800

    reponsive landing

--- src/components/about/Landing.jsx
+++ src/components/about/Landing.jsx
@@ -5,7 +5,7 @@ import Button from "../about/Button.jsx";
 const Landing = () => {
   return (
     <div className="flex justify-center items-center h-[90vh] md:h-[70vh] w-full">
-      <div className="h-auto flex sm:flex-row flex-col gap-8 items-center md:w-2/3">
+      <div className="h-auto flex sm:flex-row flex-col gap-8 justify-center items-center md:w-2/3 ">
         <div className="  flex  items-center mr-2 md:m-0">
           <Image src={chart} className="h-full w-auto m-0" alt="chart" />
         </div>
@@ -13,18 +13,16 @@ const Landing = () => {
           <p className="text-5xl !text-center md:!text-left py-1.5 font-bold text-transparent bg-clip-text bg-gradient-to-r from-hiss-blue to-hiss-purple">
             Highlander Statistics Society
           </p>
-          <div className="flex flex-col">
-            <p className="md:m-0 text-white px-0 py-2.5 font-bold  text-center sm:text-3xl md:h-14 md:w-4/5 bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full items-start">
-              at Unversity of California Riverside
-            </p>
+          <p className="md:m-0 text-white px-0 py-2.5 font-bold  text-center sm:text-3xl md:h-14 md:w-4/5 bg-gradient-to-r from-hiss-blue to-hiss-purple rounded-full items-start">
+            at Unversity of California Riverside
+          </p>
 
-            <p className="md:my-1 py-1 md:w-2/3 md:!text-left text-base md:text-xl !text-center ">
-              HiSS is the go-to club for statistics enthusiasts at UCR,
-              fostering a dynamic community through workshops and socials.
-            </p>
-            <div className="flex flex-col md:items-end items-center md:mt-[10%] md:mr-[20%]">
-              <Button text={"JOIN US"} />
-            </div>
+          <p className="md:my-1 py-1 md:w-2/3 md:!text-left text-base md:text-xl !text-center ">
+            HiSS is the go-to club for statistics enthusiasts at UCR, fostering
+            a dynamic community through workshops and socials.
+          </p>
+          <div className="flex flex-col md:items-end items-center md:mt-[10%] md:mr-[20%]">
+            <Button text={"JOIN US"} />
           </div>
         </div>
       </div>
